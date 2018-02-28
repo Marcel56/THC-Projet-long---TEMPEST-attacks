fa96ccb4f66741dfee2e0ee82a8e30ff430fe53a
Marcel56 <florian.barbarin@gmail.com>
2018-02-28 19:35:57 +0100

    modifs mov_data

--- Transmitter/RAM/mov_data.c
+++ Transmitter/RAM/mov_data.c
@@ -4,8 +4,11 @@
 #include <unistd.h>
 #include <time.h>
 #include <math.h>
+#include <string.h>
 
-float TX_TIME = 0.5;
+float TX_TIME = 0.5; //Temps de transmission d'un bit
+int DATA_SIZE = 128; //Taille maximale en octet des données à transmettre
+
 
 int * allocate_data(int buffer_size){
 
@@ -25,6 +28,7 @@ int * allocate_data(int buffer_size){
   return ptr;
 }
 
+
 void mov_data(int * data_ptr,char * buffer_ptr )
 {
   __asm("movq %0,%%xmm0\n"
@@ -39,34 +43,62 @@ void mov_data(int * data_ptr,char * buffer_ptr )
   __asm("movntdq %%xmm0,%0\n" : "=m"(*buffer_ptr));
 }
 
-int main (void)
+
+int main (int argc, char ** argv)
 {
-  int i;
-  int * data = allocate_data(4096);
-  char * memory_buffer = malloc(16);
-  //printf("Memory buffer address : %p\n", memory_buffer);
+  //Lecture du pass dans le fichier passé en argument
+  int i=1;
+  FILE * fd;
+  int c;
+  int true_data_size;
+  char tx_data[DATA_SIZE];
 
-  int * tx_data = allocate_data(sizeof(int));
-  printf("Data : %d\n", *tx_data);
-  unsigned bit = 0 ;
-  unsigned mask = 1 ;
-  for(i=0; i<sizeof(int)*8; i++){
-    bit = (*tx_data & mask) >> i ;
-    printf("%d", bit) ;
-    mask <<= 1 ;
+  if(argc == 2){
+    fd = fopen(argv[1], "r");
+
+    if (fd != NULL) {
+      tx_data[0] = 0xcc; //Création d'un header
+      while((c = fgetc(fd)) != EOF){
+        tx_data[i] = (char) c;
+        i++;
+      }
+      true_data_size = i;
+    } else {
+      printf("%s : No such file\n", argv[1]);
+      return -1;
+    }
+    fclose(fd);
+  } else {
+    printf("No file specified\n");
+    return -1;
   }
-  printf("\n");
 
-  while(1){
-    time_t start=time(NULL);
-    printf("ON! \n");
-    while(time(NULL)-start<TX_TIME){
-      for(i=0;i<4096;i+=4){
-        mov_data(data+i, memory_buffer);
+
+  //Envoie des données binaires
+  int * data_to_move = allocate_data(4096); //Création d'un buffer de données à écrire
+  char * memory_buffer = malloc(16); //Création d'un buffer dans lequel les données seront écrites
+  int j;
+
+  for(i=0;i<true_data_size; i++){
+    for(j = 7; j >= 0; --j){
+      if(((tx_data[i] & (1 << j)) ? 1 : 0) == 1){
+        //Emission pendant TX_TIME
+        time_t start=time(NULL);
+        while(time(NULL)-start<TX_TIME){
+          for(i=0;i<4096;i+=4){
+            mov_data(data_to_move+i, memory_buffer);
+          }
+        }
+        putchar('1');
+      } else {
+        //Non-émission pendant TX_TIME
+        usleep(TX_TIME*pow(10,6));
+        putchar('0');
       }
     }
-    printf("OFF! \n");
-    usleep(TX_TIME*pow(10,6));
+    putchar(' ');
   }
+  putchar('\n');
+
   return 0;
 }
