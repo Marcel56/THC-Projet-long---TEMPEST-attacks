18e78d7573a0e962859b8ae4231c86d9793c334b
Maxime G <gaglimax@gmail.com>
2018-03-01 12:07:37 +0100

    demod presque final

--- Transmitter/RAM/demod2.c
+++ Transmitter/RAM/demod2.c
@@ -2,10 +2,12 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <math.h>
 
-#define TAILLE_MAX 200
+#define TAILLE_MAX 1000
 
 char * rem_space(char *chaine)
+// Enlève les espaces d'un string
 {
   int i;
   int j = -1;
@@ -16,12 +18,52 @@ char * rem_space(char *chaine)
   return(chaine);
 }
 
-int main(void)
+int bin_to_dec(char * bin)
+// Convertit un binaire en décimal
 {
-    int c;
-    char chaine[TAILLE_MAX], tmp1[TAILLE_MAX], tmp2[TAILLE_MAX];
-    FILE *sortie = popen("xxd -b test", "r");
-    FILE *fic = fopen("sortie", "w");
+  int i, b, dec=0;
+  char c;
+  for(i=7; i>=0; i--)
+  {
+    c = *(bin+i);
+    b = c - 48;
+    dec += b * pow(2, 7-i);
+  }
+  return(dec);
+}
+
+void bin_to_ascii(char * bin, char * ascii)
+// Convertit un binaire en ascii
+{
+  int i, dec;
+  char tmp[TAILLE_MAX]="", c;
+  for(i=0; i<strlen(bin); i+=8)
+  {
+    strncpy(tmp, bin+i, 8);
+    //printf("%c\n", *(tmp+1));
+    dec = bin_to_dec(tmp);
+    c = dec;
+    ascii[i/8] = c;
+  }
+  ascii[strlen(bin)/8] = '\0';
+}
+
+int main(int argc, char ** argv)
+{
+    char chaine[TAILLE_MAX],
+         commande[30] = "xxd -b ",
+         tmp1[TAILLE_MAX],
+         tmp2[TAILLE_MAX],
+         *tmp3, *tmp4,
+         tmp5[TAILLE_MAX]="",
+         bin[TAILLE_MAX]="",
+         res[TAILLE_MAX]="",
+         *pattern="11111111",
+         ascii[TAILLE_MAX]="";
+    FILE *sortie;
+
+    strcat(commande, argv[1]);
+    sortie = popen(commande, "r");
     if(sortie == NULL)
     {
         perror("Erreur popen ");
@@ -34,8 +76,16 @@ int main(void)
       memset (tmp1, 0, sizeof (tmp1));
       strncpy(tmp1, chaine+10, 55);
       strcpy(tmp2, rem_space(tmp1));
-      fwrite(tmp2, strlen(tmp2), 1, fic);
+      strcat(bin, tmp2);
     }
+    tmp3 = strstr(bin, pattern);
+    printf("bin = %s\n", bin);;
+    strcpy(tmp5, tmp3+strlen(pattern));
+    tmp4 = strstr(tmp5, pattern);
+    strncpy(res, tmp5, tmp4-tmp5);
+    printf("res = %s\n", res);
+    bin_to_ascii(res, ascii); //remplacer bin par res
+    printf("ascii = %s\n", ascii);
     pclose(sortie);
     fclose(fic);
 
